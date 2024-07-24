2217fd58c400ee059aaf06ca4d5d283dc3527696
Sishuo Wang <sishuo.wang@aol.com>
2024-07-24 12:58:53 +0800

    Update 5.4a.R

--- CME2006/C5 Bayesian methods/scripts/5.4a.R
+++ CME2006/C5 Bayesian methods/scripts/5.4a.R
@@ -2,8 +2,9 @@
 
 
 ########################################
-lnprior <- function(){
-	log(dunif(d,0,100))
+# assume branch_length ~ Uniform(0,10)
+lnprior_unif <- function(d){
+	log(dunif(d,0,10))
 }
 
 lnl_JC69 <- function(d, n, x){
@@ -13,15 +14,26 @@ lnl_JC69 <- function(d, n, x){
 do_mcmc <- function(w, nsample, n, x){
     ds <- numeric(nsample)
     accepts <- numeric(nsample)
-    d <- 0.8; lnl <- lnl_JC69(d, n, x)
+    d <- 0.8
+	
+	# log-prior
+	lnprior <- lnprior_unif(d)
+	
+	# log-likelihood
+	lnl <- lnl_JC69(d, n, x)
+	
+	# log-posterior
+	lnposterior <- lnprior + lnl
+	
     for(i in 2:nsample){
         d_new <- runif(1, d-w/2, d+w/2)
         d_new <- ifelse(d_new>=0, d_new, d)
-        lnl_new <- lnl_JC69(d_new, n, x)
-        alpha <- min(1, exp(lnl_new-lnl))
+		
+        lnposterior_new <- lnprior_unif(d_new) + lnl_JC69(d_new, n, x)
+        alpha <- min(1, exp(lnposterior_new-lnposterior))
         if(runif(1) < alpha){
             d <- d_new
-            lnl <- lnl_new
+            lnposterior <- lnposterior_new
             accepts[i] <- 1
         }
         ds[i] <- d
